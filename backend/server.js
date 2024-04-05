b05a7d4c3f6ef41d3f93bbc216a019ca8a17bf6a
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-04-05 19:42:00 +0530

    package.json change

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectToDb from "./connectToDb.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -11,12 +12,16 @@ const app = express();
 
 app.use(express.json());
 
+const __dirname = path.resolve();
+
 
 app.use("/api/msg", msgRouter);
 
-app.get("/",(req,res)=>{
-  res.send("kjsfdalf;");
-})
+// app.get("/",(req,res)=>{
+//   res.send("kjsfdalf;");
+// })
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 
 app.listen(port, async () => {
